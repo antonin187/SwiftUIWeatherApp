21c4104a85821c9290a937f764fb5e93df08576c
Antonin SIMON <antonin.simon@epsi.fr>
2023-10-07 21:06:21 +0200

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Antonin Simon on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
